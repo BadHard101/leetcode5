c01078cee6096380a523c70e63cf7b79b20687df
BadHard <BadHard101@yandex.ru>
2024-08-02 05:04:44 +0300

    done

--- src/Solution.java
+++ src/Solution.java
@@ -2,32 +2,40 @@ class Solution {
     //         8 10     l 
     // sdrfaafsdfdsghbababghdsdfger
     //                    r  
+    private int max;
+    private String ans;
+    private char[] chars;
+    int n;
+
+    private void findPalindromic(int l, int r, String s) {
+        //    l
+        // abcc
+        //    r
+        while (l >= 0 && r < n && chars[l] == chars[r]) {
+            l--;
+            r++;
+        }
+        l++;
+        r--;
+
+        if (r - l + 1 > max) {
+            max = r - l + 1;
+            ans = s.substring(l, r + 1);
+        }
+    }
+
     public String longestPalindrome(String s) {
         if (s.length() == 1) return s;
 
-        char[] chars = s.toCharArray();
-        int n = chars.length();
-        int max = 1;
-        String ans = s.charAt(0);
+        chars = s.toCharArray();
+        n = chars.length;
+        max = 1;
+        ans = Character.toString(s.charAt(0));
 
         for (int i = 0; i < s.length(); i++) {
-            if (chars[i] == chars[i+1]) {
-                int l = i;
-                int r = i+1;
-                //
-            }
-            else if (i != 0 && i != n-1) {
-                int l = i - 1;
-                int r = i + 1;
-                while (l >= 0 && r < n && chars[l] == chars[r]) {
-                    l--;
-                    r++;
-                }
-                if (r - l + 1 > max) {
-                    max = r - l + 1;
-                    ans = s.substring(l, r + 1);
-                }
-            }
+            findPalindromic(i, i+1, s);
+            findPalindromic(i-1,i+1, s);
         }
+        return ans;
     }
 }
